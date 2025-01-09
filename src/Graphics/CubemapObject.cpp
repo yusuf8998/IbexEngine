daa7a3818a74bc5cf52b34a159f677b50ed6e7ef
yusuf8998 <karamustafa1725@gmail.com>
2025-01-09 16:39:53 +0300

    Update CubemapObject to use GL_CLAMP_TO_EDGE for texture wrapping parameters

--- src/Graphics/CubemapObject.cpp
+++ src/Graphics/CubemapObject.cpp
@@ -25,9 +25,9 @@ void CubemapObject::generateCubemap(unsigned char *data[6], int width, int heigh
     glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
     // Set the texture parameters
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
