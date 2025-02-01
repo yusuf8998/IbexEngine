c0159186a4731237e792d468d01a60abf9530a72
yusuf8998 <karamustafa1725@gmail.com>
2025-02-01 03:56:52 +0300

    Clean up skybox vertex shader by removing unused inputs and simplifying main function

--- res/Shaders/Shader_Cube/vertex_cube.glsl
+++ res/Shaders/Shader_Cube/vertex_cube.glsl
@@ -1,16 +1,13 @@
 #version 450
 
 layout(location = 0) in vec3 v_position;
-layout(location = 1) in vec2 v_uv;
-layout(location = 2) in vec3 v_normal;
 
 layout(location = 0) out vec3 texCoords;
 
 uniform mat4 projection;
 uniform mat4 view;
 
-void main()
-{
+void main() {
     mat4 rot_view = mat4(mat3(view));
     vec4 pos = projection * rot_view * vec4(v_position, 1);
     gl_Position = pos.xyww;
